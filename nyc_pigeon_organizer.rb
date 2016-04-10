05a8d3e562ffaeb5b508d26a496258dc871f3952
danielperzales <dani3l122@hotmail.com>
2016-04-10 19:22:42 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-end
+pidg_hash_all = {}
+  
+ 
+   data.each do |attribute, dataHash|
+     # attribute = color, gender, lives
+     
+    dataHash.each do |attributeValue, nameArray|
+     # attributeValues = purple, grey, male, "Subway", etc, or data[:attribute]
+         nameArray.each do |name|
+             pidg_hash_all_data = pidg_hash_all[name] # creates hash where name is key
+             if pidg_hash_all_data == nil
+                 pidg_hash_all_data = {}
+                 pidg_hash_all_data[:color] = []
+                 pidg_hash_all_data[:gender] = []
+                 pidg_hash_all_data[:lives] = []
+             end
+             pidg_hash_all_data[attribute] << attributeValue.to_s
+             pidg_hash_all[name] = pidg_hash_all_data
+         end
+     end
+   end
+   pidg_hash_all
+  end
+
+
+#67
